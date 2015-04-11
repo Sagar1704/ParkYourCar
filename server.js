c6609d179a3571ed83fae9cdbd1e7de4759b13d4
Sagar Deshpande <deshpandesagar1704@gmail.com>
2015-04-11 17:14:25 -0400

    Basic homepage and other pages

---
+++ server.js
@@ -0,0 +1,52 @@
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var path     = require('path');
+var port     = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var cons     = require('consolidate');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', cons.swig);
+app.set('view engine', 'html'); // set up ejs for templating
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		title : 'Park Your Car',
+		logo : 'resources/lamborgini.jpg'
+	});
+});
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
