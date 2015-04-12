2c1e2fe774ab420b26f6328a4c42abe5ee9485e2
Sagar Deshpande <deshpandesagar1704@gmail.com>
2015-04-12 02:38:21 -0400

    Local Signup and login done

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,17 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.html'); // load the index.html file
+        //res.render('index.html'); // load the index.html file
+            res.render('index.ejs', {
+            title : 'Park Your Car',
+            logo : 'images/lamborgini.jpg',
+            description : 'Cool website to help you park your Car!!',
+            about : '',
+            rules : '',
+            akshay : 'Akshay Thakare(asd@utdallas.edu)',
+            aniket : 'Aniket Rahate(axr@utdallas.edu',
+            sagar : 'Sagar Deshpande(ssd140830@utdallas.edu)'
+        });
     });
 
     // =====================================
@@ -14,11 +24,15 @@ module.exports = function(app, passport) {
     app.get('/login', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('login.html', { message: req.flash('loginMessage') }); 
+        res.render('login.ejs', { message: req.flash('loginMessage') }); 
     });
 
     // process the login form
-    // app.post('/login', do all our passport stuff here);
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     // =====================================
     // SIGNUP ==============================
@@ -27,11 +41,19 @@ module.exports = function(app, passport) {
     app.get('/signup', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('signup.html', { message: req.flash('signupMessage') });
+        res.render('signup.ejs', { 
+            message: req.flash('signupMessage'),
+            title : 'Park Your Car',
+            logo : 'images/lamborgini.jpg' 
+        });
     });
 
     // process the signup form
-    // app.post('/signup', do all our passport stuff here);
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     // =====================================
     // PROFILE SECTION =====================
@@ -39,7 +61,7 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.html', {
+        res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
@@ -50,6 +72,13 @@ module.exports = function(app, passport) {
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
+    });
+
+    // =====================================
+    // ERROR ===============================
+    // =====================================
+    app.get('/error', function(req, res) {
+        res.render('error.ejs', {})
     });
 };
 
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var port     = process.env.PORT || 3000;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-var cons     = require('consolidate');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -19,7 +18,16 @@ var configDB = require('./config/database.js');
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+mongoose.connection.on('open', function (ref) {
+	console.log('Connected to mongo server.');	
+});
+mongoose.connection.on('error', function (err) {
+	console.log('Could not connect to mongo server!');
+	console.log(err);
+});
+
+
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -27,25 +35,32 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', cons.swig);
-app.set('view engine', 'html'); // set up ejs for templating
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	res.render('index', {
-		title : 'Park Your Car',
-		logo : 'resources/lamborgini.jpg'
-	});
-});
+app.set('views', path.join(__dirname, 'views'));
+//app.engine('html', cons.swig);
+app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: 'parkyourcaratyourownrisk' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// Handle 404
+app.use(function(req, res) {
+	res.status(400);
+	res.render('404.ejs', {title: '404: File Not Found'});
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+	res.status(500);
+	res.render('500.ejs', {title: '500: Internal Server Error', error: error});
+});
 
 // launch ======================================================================
 app.listen(port);
